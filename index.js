87923666e99c4c7c82a150e449f867b4f92d51eb
c4chamok <c4chamok@gmail.com>
2025-01-15 13:48:54 +0600

    JWT cookie system implemented on server

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express =  require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-crud1.0ugo3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-CRUD1`;
+
+
 
 const app = express();
 
@@ -20,6 +24,50 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '5h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            })
+                .send({ success: true });
+        });
+
+
+
+
+
+
+    }finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
 
 
 
